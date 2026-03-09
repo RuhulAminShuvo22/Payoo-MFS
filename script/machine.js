b5ddc29aef314bfad9ce920ad20df81b0f756576
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-10 00:45:12 +0600

    matchine part done

--- script/machine.js
+++ script/machine.js
@@ -14,3 +14,23 @@ function getBalance() {
   console.log("current balance", Number(balance));
   return Number(balance);
 }
+// machine value -> set Balance
+
+function setBalance(value) {
+  const balanceElement = document.getElementById("balance");
+  balanceElement.innerText = value;
+}
+
+//machine id > hide all > show id
+function showOnly(id) {
+  const addmoney = document.getElementById("add-money");
+  const cashout = document.getElementById("cashout");
+  //   console.log(`add Money- ${addmoney} , Cashpout - ${cashout}`);
+  //shobai k hide kore dao
+  addmoney.classList.add("hidden");
+  cashout.classList.add("hidden");
+
+  //id wala element ta ke tumi show kora
+  const selected = document.getElementById(id);
+  selected.classList.remove("hidden");
+}
