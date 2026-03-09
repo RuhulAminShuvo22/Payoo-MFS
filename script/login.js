e2c88a8c80e1821dcc8b44771406fecbc9697978
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-10 00:43:17 +0600

    login part done

--- script/login.js
+++ script/login.js
@@ -10,4 +10,17 @@ document.getElementById("login-btn").addEventListener("click", function () {
   const inputPin = document.getElementById("input-pin");
   const pin = inputPin.value;
   console.log(pin);
-  
+   //3- match pin & mobile number
+  if (contactNumber == "01234567890" && pin == "1234") {
+    //3-1 true:::>> alert> homepage
+    alert("login Success");
+
+    // window.location.replace("/home.html");
+    window.location.assign("/home.html");
+  } else {
+    //3-2 false:::>> alert> return
+    alert("login Failed");
+    return;
+  }
+});
+
