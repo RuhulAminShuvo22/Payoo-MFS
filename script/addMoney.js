06755f65bacd2ef5acb63806566923496de4b235
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-10 00:14:55 +0600

    add money done

--- script/addMoney.js
+++ script/addMoney.js
@@ -11,3 +11,20 @@ document.getElementById("add-money-btn").addEventListener("click", function () {
     alert("invalid acc no");
     return;
   }
+    //3- get ammount
+  const amount = getValueFromInput("add-money-amount");
+  const currentBalance = getBalance();
+  const newBalance = currentBalance + Number(amount);
+
+  const pin = getValueFromInput("add-money-pin");
+  if (pin == "1234") {
+    alert(`Add Money Success from
+         ${bankAccount} 
+         at ${new Date()}`);
+    setBalance(newBalance);
+  } else {
+    alert("Invalid Pin");
+
+    return;
+  }
+});
