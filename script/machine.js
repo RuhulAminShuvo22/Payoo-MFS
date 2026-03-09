8b6d32153b1c11afa9f25183e07bf17a76a95c25
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-10 00:44:19 +0600

    matchine part half

---
+++ script/machine.js
@@ -0,0 +1,16 @@
+console.log("Machine Added");
+
+//machine id -> input value
+function getValueFromInput(id) {
+  const input = document.getElementById(id);
+  const value = input.value;
+  console.log(id, value);
+  return value;
+}
+//machine -> balance
+function getBalance() {
+  const balanceElement = document.getElementById("balance");
+  const balance = balanceElement.innerText;
+  console.log("current balance", Number(balance));
+  return Number(balance);
+}
